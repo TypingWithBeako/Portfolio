cc3f8d2561caa5efa28c9161a85b45b696242d84
TypingWithBeako <thaituan10150806@gmail.com>
2025-05-09 13:37:25 +0700

    feat: Added image preview using PhotoSwipe and finish projects section

--- assets/js/animateonscroll.js
+++ assets/js/animateonscroll.js
@@ -1,7 +1,7 @@
 AOS.init({
     duration: 800,       // Animation duration in milliseconds
     easing: 'ease-out',  // Animation timing function
-    once: false          // Whether animation should happen only once
+    once: false,          // Whether animation should happen only once
   });
   if ('scrollRestoration' in history) {
     history.scrollRestoration = 'manual';
